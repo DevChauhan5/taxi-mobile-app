5b1816e4e85063438b2037f6302ef888930fb8a8
Dev Chauhan <devchauhanthedev@gmail.com>
2024-08-18 20:14:33 +0530

    deleted some files and added assets

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const index = () => {
+  return (
+    <SafeAreaView>
+        <Text>Home Here</Text>
+    </SafeAreaView>
+  )
+}
+
+export default index
